a246aa20d351771482916c90af177ccf9aa0caad
jry2112 <jry2112@columbia.edu>
2021-05-23 13:26:51 -0400

    email

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
-var path = require('path');
+
 var app = express();
 app.set('port', process.env.PORT || 3000 || 80);
+
+var path = require('path');
+var nodemailer = require('nodemailer');
+
+var bodyParser = require('body-parser');
+app.use(express.urlencoded({extended: true}));
 
 // Configure template Engine and Main Template File
 app.engine('.hbs', exphbs({
@@ -17,8 +23,7 @@ app.set('view engine', '.hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-// routes
+// routes for pages
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -31,6 +36,51 @@ app.get('/store', (req, res) => {
 app.get('/support', (req, res) => {
     res.render('support');
 });
+
+// wrapper for await
+async function main() {
+    // route for sending email through form using Nodemailer
+    // Source --> https://tylerkrys.ca/blog/adding-nodemailer-email-contact-form-node-express-app
+    app.post('/', (req, res) => {
+        // Test email account
+        let testAccount = await nodemailer.createTestAccount();
+
+        // Create the SMTP transporter object
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: testAccount.user,
+                pass: testAccount.pass
+            }
+        });
+
+        // Email specifics
+        var mailOpts = {
+            from: testAcount.user,
+            to: testAccount.user,
+            subject: 'New message from contact form at DCNR1',
+            text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+        };
+
+        // Attempt to send the email
+        smtpTrans.sendMail(mailOpts, (error, response) => {
+            if (error) {
+                res.render('contact-failure') // Show a page indicating failure
+            }
+            else {
+                res.render('contact-success') // show a page indicating success
+            }
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        
+        // Preview URL for Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    }); 
+}
+
+main().catch(console.error);
 
 // port where app is served
 app.listen(app.get('port'), () => {
